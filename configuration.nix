d429916a00abbc44ff7ca74711ec8cc9f15a1766
NostraDavid <NostraDavid@gmail.com>
2025-06-27 19:49:55 +0200

    add a whole bunch of applications; nvidia drivers

--- configuration.nix
+++ configuration.nix
@@ -99,22 +99,49 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  
+
     # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     alacritty
+    bat
+    mpv
+    fd
+    gcc
     git
+    keepassxc
+    lazygit
+    lf
+    ncdu
+    ripgrep
+    tmux
+    unzip
+    uv
+    wezterm
     wl-clipboard
+    vscode
+    slack
+    legcord
+    synology-drive-client
+    gparted
+    podman
+    k3s
+    inxi
     (neovim.overrideAttrs (old: {
       # Add wl-clipboard as a build input to Neovim
       nativeBuildInputs = old.nativeBuildInputs ++ [ wl-clipboard ];
     }))
-    wezterm
-    ripgrep
-    fd
-    unzip
-    gcc
+  ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -144,5 +171,42 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
-}
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
