65d2db553234a604bdde2ee8b3fd8e8caa4b2b8b
NostraDavid <NostraDavid@gmail.com>
2025-06-29 18:41:43 +0200

    set hostname

--- configuration.nix
+++ configuration.nix
@@ -15,8 +15,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "odin";
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
