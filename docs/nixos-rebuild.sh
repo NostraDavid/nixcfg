6cf1a15b4b75a97eea0bf5094abdaba8297dde04
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2025-07-13 15:28:39 +0200

    add command to install the local configuration.nix
    This way, I won't need to symlink anymore.

--- docs/nixos-rebuild.sh
+++ docs/nixos-rebuild.sh
@@ -3,6 +3,9 @@
 # == build the new configuration - /etc/nixos/configuration.nix ==
 sudo nixos-rebuild switch
 
+# == update local nixos configuration ==
+time sudo nixos-rebuild switch -I nixos-config=configuration.nix
+
 # == test the configuration change, until reboot ==
 nixos-rebuild test
 
@@ -12,4 +15,3 @@ nixos-rebuild repl
 # == build a vm ==
 nixos-rebuild build-vm
 ./result/bin/run-*-vm
-
