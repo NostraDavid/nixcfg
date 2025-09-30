287f019475f490739a059780303948159cb84bfe
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2025-09-30 11:09:16 +0200

    enable podman for frigg

--- hosts/frigg/configuration.nix
+++ hosts/frigg/configuration.nix
@@ -177,6 +177,20 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # # Enable common container config files in /etc/containers
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment = {
