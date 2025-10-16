8f9e7c8751788c9045ad0abea815a21ec650c1af
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2025-10-16 22:22:41 +0200

    improve the dotfiles to symlink directly, instead of breaking it by linking from the store

--- modules/dotfiles.nix
+++ modules/dotfiles.nix
@@ -1,48 +1,35 @@
 # home/common.nix
 # Shared home-manager configuration for all hosts.
-{pkgs, ...}: {
+{config, ...}: {
   # This list may look a little weird, but that's because the original dotfiles
   # were managed by `stow`, which needs this folder structure to work correctly.
   # I decided to keep it that way, so I could return to stow in the future.
-  home.file = {
-    ".bash_aliases".source = ../dotfiles/bash-5.2.37/.bash_aliases;
-    ".bash_profile".source = ../dotfiles/bash-5.2.37/.bash_profile;
-    ".bashrc".source = ../dotfiles/bash-5.2.37/.bashrc;
-    ".config/bat/config".source = ../dotfiles/bat-0.25.0/.config/bat/config;
-    ".config/neovim/" = {
-      source = ../dotfiles/neovim-0.11/.config/neovim;
-      recursive = true;
-    };
-    ".config/pip/pip.conf".source = ../dotfiles/pip-22+/.config/pip/pip.conf;
-    ".config/powerline" = {
-      source = ../dotfiles/powerline-bash/.config/powerline;
-      recursive = true;
-    };
-    ".config/pypoetry/" = {
-      source = ../dotfiles/pypoetry-2.1/.config/pypoetry;
-      recursive = true;
-    };
-    ".config/pypoetry/config.toml".source = ../dotfiles/pypoetry-2.1/.config/pypoetry/config.toml;
-    ".config/starship.toml".source = ../dotfiles/starship-1.23.0/.config/starship.toml;
-    ".config/wezterm/wezterm.lua".source = ../dotfiles/wezterm-0-unstable-2025-05-18/.config/wezterm/wezterm.lua;
-    ".git-templates" = {
-      source = ../dotfiles/git-templates/.git-templates;
-      recursive = true;
-    };
-    ".gitconfig".source = ../dotfiles/git/.gitconfig;
-    ".groovylintrc.json".source = ../dotfiles/groovy-lint/.groovylintrc.json;
-    ".inputrc".source = ../dotfiles/readline/.inputrc;
-    ".tmux.conf".source = ../dotfiles/tmux-3.5a/.tmux.conf;
-    ".vim" = {
-      source = ../dotfiles/vim-9.0/.vim;
-      recursive = true;
-    };
-    ".viminfo".source = ../dotfiles/vim-9.0/.viminfo;
-    ".vimrc".source = ../dotfiles/vim-9.0/.vimrc;
-    "dev/find-uncommitted.sh".source = ../dotfiles/dev/find-uncommitted.sh;
-    "dev/grab.sh".source = ../dotfiles/dev/grab.sh;
-    "dev/grab_parallel.sh".source = ../dotfiles/dev/grab_parallel.sh;
-    ".local/bin/folder_stats".source = ../dotfiles/scripts/folder_stats;
+  home.file = let
+  dot = "${config.home.homeDirectory}/dev/NostraDavid/nixcfg/dotfiles";
+    mk = path: config.lib.file.mkOutOfStoreSymlink path;
+  in {
+    ".bash_aliases".source = mk "${dot}/bash-5.2.37/.bash_aliases";
+    ".bash_profile".source = mk "${dot}/bash-5.2.37/.bash_profile";
+    ".bashrc".source = mk "${dot}/bash-5.2.37/.bashrc";
+    ".config/bat/config".source = mk "${dot}/bat-0.25.0/.config/bat/config";
+    ".config/neovim/".source = mk "${dot}/neovim-0.11/.config/neovim";
+    ".config/pip/pip.conf".source = mk "${dot}/pip-22+/.config/pip/pip.conf";
+    ".config/powerline".source = mk "${dot}/powerline-bash/.config/powerline";
+    ".config/pypoetry/".source = mk "${dot}/pypoetry-2.1/.config/pypoetry";
+    ".config/starship.toml".source = mk "${dot}/starship-1.23.0/.config/starship.toml";
+    ".config/wezterm/wezterm.lua".source = mk "${dot}/wezterm-0-unstable-2025-05-18/.config/wezterm/wezterm.lua";
+    ".git-templates".source = mk "${dot}/git-templates/.git-templates";
+    ".gitconfig".source = mk "${dot}/git/.gitconfig";
+    ".groovylintrc.json".source = mk "${dot}/groovy-lint/.groovylintrc.json";
+    ".inputrc".source = mk "${dot}/readline/.inputrc";
+    ".tmux.conf".source = mk "${dot}/tmux-3.5a/.tmux.conf";
+    ".vim".source = mk "${dot}/vim-9.0/.vim";
+    ".viminfo".source = mk "${dot}/vim-9.0/.viminfo";
+    ".vimrc".source = mk "${dot}/vim-9.0/.vimrc";
+    "dev/find-uncommitted.sh".source = mk "${dot}/dev/find-uncommitted.sh";
+    "dev/grab.sh".source = mk "${dot}/dev/grab.sh";
+    "dev/grab_parallel.sh".source = mk "${dot}/dev/grab_parallel.sh";
+    ".local/bin/folder_stats".source = mk "${dot}/scripts/folder_stats";
   };
 
   home.sessionVariables = {};
