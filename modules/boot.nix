da6ef08a846eba14f213a05485b150eab709a6ae
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2025-10-22 15:27:35 +0200

    set port 80 to be unpriviliged for Traefik reverse proxy

--- modules/boot.nix
+++ modules/boot.nix
@@ -6,5 +6,12 @@
 
     # Enable USB automounting for external drives.
     supportedFilesystems = ["exfat" "ntfs"];
+
+    kernel = {
+      sysctl = {
+        # for project ctb; Traefik needs to bind to low ports
+        "net.ipv4.ip_unprivileged_port_start" = 80;
+      };
+    };
   };
 }
