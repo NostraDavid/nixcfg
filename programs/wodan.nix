a781bdbef9d8ca1c28211dfee04b4b29e6c6012e
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-01-26 22:44:39 +0100

    move opencode from shared to wodan

--- programs/wodan.nix
+++ programs/wodan.nix
@@ -87,6 +87,7 @@ in {
     pkgs-unstable.stable-diffusion-cpp # Stable Diffusion in C++
     pkgs-unstable.vllm # High-performance inference server for large language models
     pkgs-unstable.antigravity # Google IDE
+    pkgs-unstable.opencode
     # pkgs-unstable.ollama-cuda # Local LLM server
     # # Zed is slow to build :/
     # (pkgs-unstable.zed-editor.overrideAttrs (_: {
