e4ff82de36340b01356a8375ec91c2cb8657ddbd
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-02-27 10:33:10 +0100

    add devenv (not direnv - already have that)

--- programs/shared.nix
+++ programs/shared.nix
@@ -37,6 +37,7 @@ in {
     csvkit # Python based CSV toolkit (heavier)
     curl
     delta # diff viewer
+    devenv # Development environment manager
     diff-so-fancy # diff viewer
     difftastic # diff viewer
     diffutils # Diff
