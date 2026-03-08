fba6e2c908ac89362cd879105ba7e28e24cc9d58
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-03-08 11:25:18 +0100

    add plasma-manager and integrate desktop entries for Firefox ESR and Visual Studio Code

--- flake.nix
+++ flake.nix
@@ -8,6 +8,11 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
     home-manager-unstable = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
