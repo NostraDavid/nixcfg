49b3dd21b83bc12e5e3a36a3e4f2734d5bdcb632
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-03-09 22:53:12 +0100

    move packages around; some to wodan, some become unstable instead of local, etc.

--- programs/wodan.nix
+++ programs/wodan.nix
@@ -4,7 +4,7 @@
   inputs,
   ...
 }: let
-  pkgs-unstable = import inputs.nixpkgs-unstable {
+  unstable = import inputs.nixunstable {
     inherit (pkgs.stdenv.hostPlatform) system;
     config = pkgs.config // {allowUnfree = true;};
   };
@@ -13,7 +13,7 @@
     entries = readDir ../pkgs;
   in
     filter (name: entries.${name} == "directory") (attrNames entries);
-  pkgs-local =
+  local =
     listToAttrs
     (map (name: {
         inherit name;
@@ -391,9 +391,15 @@ in {
 
   home.packages = with pkgs; [
     # Wodan-specific Terminal packages go here
+    amfora # Gemini Protocol Client (TUI)
+    bombadillo # Gemini Protocol Client (TUI)
+    kristall # Gemini Protocol Client
+    lagrange # Gemini Protocol Client
+
     exfatprogs # ExFAT FS utilities
     helm
     k3d # k3s in docker
+    flite # flite -f <file>; TTS Engine
     k3s # kubes (includes kubectl)
     postgresql # for psql; there's pgcli for shared
     redpanda-client # Kafka alternative
@@ -414,66 +420,69 @@ in {
     }))
 
     # Wodan-specific GUI packages go here
+    anki # Flashcard app
     anydesk
     blender
+    dbeaver-bin # Database management tool
     guacamole-client
     guacamole-server
-    nomachine-client
-    rustdesk
-    rustdesk-server
-    xrdp # Remote Desktop Protocol client
-    anki # Flashcard app
-    dbeaver-bin # Database management tool
     itch # Game launcher
     libreoffice-qt6 # Office suite
+    nomachine-client
     nuclear # Music player
+    renderdoc # Graphics debugger
+    rustdesk
+    rustdesk-server
     slack # Slack messaging app
     wireguard-ui # WireGuard UI
+    xrdp # Remote Desktop Protocol client
 
     # GUI libs for Haemwend
-    xorg.libX11
-    libGL
-    xorg.libXrender
-    xorg.libXext
-    xorg.libXcursor
-    xorg.libXrandr
-    xorg.libXi
     fontconfig
     freetype
+    libGL
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXext
+    xorg.libXi
+    xorg.libXrandr
+    xorg.libXrender
 
     # games
+    # unstable.openra_2019-release
     endless-sky
-    pkgs-unstable.openrct2
     godot
+    unstable.openrct2
 
     # for stable-diffusion-webui
     gperftools
 
     # unstable
-    pkgs-unstable.friture # Real-time audio analyzer
-    pkgs-unstable.stable-diffusion-cpp # Stable Diffusion in C++
-    # pkgs-unstable.vllm # High-performance inference server for large language models
-    # pkgs-unstable.antigravity # Google IDE
-    # pkgs-unstable.opencode
-    # pkgs-unstable.ollama-cuda # Local LLM server
+    unstable.friture # Real-time audio analyzer
+    unstable.stable-diffusion-cpp # Stable Diffusion in C++
+    # unstable.vllm # High-performance inference server for large language models
+    # unstable.antigravity # Google IDE
+    # unstable.opencode
+    # unstable.ollama-cuda # Local LLM server
     # # Zed is slow to build :/
-    # (pkgs-unstable.zed-editor.overrideAttrs (_: {
+    # (unstable.zed-editor.overrideAttrs (_: {
     #   doCheck = false;
     # })) # Zed text editor
 
     # local
-    pkgs-local.codemogger
-    pkgs-local.stable-diffusion-webui
-    # pkgs-local.github-copilot-cli
-    pkgs-local.pixieditor
-    # pkgs-local.nanocoder
-    pkgs-local.photorec
-    # pkgs-local.opencode
-    # pkgs-local.vscode-pinned
-    # pkgs-local.synology-drive-client-pinned # kaput in 25.11
-    # pkgs-local.goose
-    # pkgs-local.bitnet
-    # pkgs-local.voxtype
-    pkgs-local.whatpulse
+    # local.bitnet
+    # local.github-copilot-cli
+    # local.goose
+    # local.nanocoder
+    # local.opencode
+    # local.synology-drive-client-pinned # kaput in 25.11
+    # local.voxtype
+    # local.vscode-pinned
+    # local.codemogger
+    local.cool-retro-term # terminal emulator with retro style
+    local.dlss-updater
+    local.photorec # image recovery
+    # local.pixieditor
+    # local.stable-diffusion-webui
   ];
 }
