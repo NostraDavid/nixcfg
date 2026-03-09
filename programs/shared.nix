0a0e73988c3359e5402899efdc7208d6190461cf
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-03-09 22:53:12 +0100

    move packages around; some to wodan, some become unstable instead of local, etc.

--- programs/shared.nix
+++ programs/shared.nix
@@ -97,11 +97,10 @@ in {
 
   home.packages = with pkgs; [
     ## Terminal apps
+    # mozjpeg # JPEG image optimizer - doesn't work with jpegli
     alejandra # nix formatter
-    amfora # Gemini Protocol Client (TUI)
     atuin # shell history manager
     bat # cat replacement
-    bombadillo # Gemini Protocol Client (TUI)
     btop # Resource monitor
     busybox
     colordiff # diff viewer
@@ -116,26 +115,22 @@ in {
     duckdb
     dust # better du called dust
     ed # The standard editor
-    msedit # Microsoft Editor
     exiftool # for image metadata manipulation
     eza # modern replacement for `ls`
     fd # sometimes also fdfind or fd-find
     ffmpeg-full
     file # file type identifier
-    flite # flite -f <file>; TTS Engine
-    freetype
     fzf # Fuzzy finder
     gcc
     gcc-unwrapped # to fix `ImportError: libstdc++.so.6: cannot open shared object file: No such file or directory` for numpy
     gdu # Disk usage analyzer with Go
     gh # GitHub CLI
-    ghostty # terminal
     git # This will now use your pinned version (2.45.0)
     git-lfs # Git Large File Storage
     glances # htop with temperature information
     gnugrep # GNU grep
     gnused # GNU sed
-    graphicsmagick
+    graphicsmagick # image processing
     hadolint # Dockerfile linter
     helix # Text editor (hx)
     home-manager # Home Manager for managing user configurations
@@ -143,7 +138,7 @@ in {
     httpie # User-friendly HTTP client
     hyperfine # Command-line benchmarking tool
     image_optim # Image optimization tool
-    imagemagick
+    imagemagick # image processing
     inetutils # telnet
     inotify-tools # Tools to watch for file system events
     inxi # system information tool
@@ -151,32 +146,29 @@ in {
     ipcalc # it is a calculator for the IPv4/v6 addresses
     jpeginfo # JPEG image validator
     jpegoptim # JPEG image optimizer
-    libjpeg_turbo # JPEG tools including jpegtran
     jq # JSON processor
     just # justcfile
     k9s # Kubernetes CLI tool
     kakoune # Text editor
     kdash # Kubernetes dashboard
     killall # kill processes by name
-    kristall # Gemini Protocol Client
-    lagrange # Gemini Protocol Client
     lazygit
     less # terminal pager
     lf # Terminal file manager
-    libpcap # for Whatpulse
+    libjpeg_turbo # JPEG tools including jpegtran
     lsd # A modern replacement for 'ls' command
     lsof # List open files
     lynx # Terminal-based web browser
     lz4 # Fastest compression algorithm
-    meld
+    meld # folder/file compare
     miller # CSV processor
     mlocate # locate command
     most # terminal pager
-    # mozjpeg # JPEG image optimizer - doesn't work with jpegli
+    msedit # Microsoft Editor
     mtr # A network diagnostic tool
     mutt # Terminal-based email client
     ncdu # Disk usage analyzer with ncurses interface
-    netcat-gnu
+    netcat-gnu # nc
     newsboat # RSS reader
     nixd # nix LSP
     nnn # Terminal file manager
@@ -196,7 +188,6 @@ in {
     powerline # The best Bash Prompt!
     procs
     pv # Pipe viewer, useful for monitoring data through a pipe
-    renderdoc # Graphics debugger
     riffdiff # diff viewer
     ripgrep # Search tool (rg)
     rsync
@@ -204,18 +195,19 @@ in {
     shellcheck
     shfmt # Shell script formatter
     smartmontools # for monitoring hard drive health
-    svgo # SVG optimizer
     sqlfluff # SQL linter and formatter
     sqlite
     starship # Shell prompt
     stow # GNU Stow for managing dotfiles
     strace
+    svgo # SVG optimizer
     sysstat # for iostat
     tmux
     tree # Display directory structure in a tree-like format
     ty # Astral type checker
     unzip
     util-linux # For `chrt` command
+    uv # Astral project manager
     viddy # Watch alternative with better color support
     visidata # Interactive terminal multitool for tabular data
     w3m # Text-based web browser
@@ -228,41 +220,7 @@ in {
     yq-go # YAML processor
     yt-dlp
     zellij # tmux alternative
-    zopfli # For zopflipng; optimize PNG files
     zoxide
-    zstd # Fast compression algorithm with better ratio than lz4; contains zstdcat for decompression
-
-    # Neovim related
-    (unstable.neovim.overrideAttrs (old: {
-      nativeBuildInputs = old.nativeBuildInputs ++ [wl-clipboard];
-    }))
-    xclip # X11 clipboard fallback for Neovim when Wayland not active
-    markdownlint-cli
-    luajit # Lua 5.1 compat
-    luajitPackages.luarocks_bootstrap
-    jdk17 # openjdk for nvim-lsp-java
-
-    # unstable
-    # unstable.openra_2019-release
-    unstable.gemini-cli
-    unstable.github-copilot-cli
-    unstable.uv
-    unstable.oxfmt # prettier replacement
-    unstable.oxlint # js linter
-    unstable.fastfetch # neofetch alternative
-    unstable.zigfetch # neofetch alternative
-    unstable.devenv # Development environment manager | using unstable for 2.x
-
-    # local
-    local.codex # Code autocompletion tool
-    local.cool-retro-term # terminal emulator with retro style
-    local.dlss-updater
-    local.jpegli
-    local.vscode-pinned
-
-    # podman
-    podman-desktop # GUI for managing containers
-    podman-compose # docker-compose alternative
 
     # compression tools
     brotli
@@ -274,35 +232,67 @@ in {
     xz
     zip
     zopfli # For zopflipng; optimize PNG files
+    zstd # Fast compression algorithm with better ratio than lz4; contains zstdcat for decompression
+
+    # Neovim related
+    (unstable.neovim.overrideAttrs (old: {
+      nativeBuildInputs = old.nativeBuildInputs ++ [wl-clipboard];
+    }))
+    jdk17 # openjdk for nvim-lsp-java
+    luajit # Lua 5.1 compat
+    luajitPackages.luarocks_bootstrap
+    markdownlint-cli
+    xclip # X11 clipboard fallback for Neovim when Wayland not active
+
+    # unstable
+    unstable.gemini-cli
+    unstable.github-copilot-cli
+    unstable.oxfmt # prettier replacement
+    unstable.oxlint # js linter
+    unstable.fastfetch # neofetch alternative
+    unstable.zigfetch # neofetch alternative
+    unstable.devenv # Development environment manager | using unstable for 2.x
+    unstable.vscode
+
+    # local
+    freetype # font-rendering library, for Whatpulse
+    libpcap # for Whatpulse
+    local.codex # Code autocompletion tool
+    local.jpegli
+    local.whatpulse
+
+    # podman
+    podman-desktop # GUI for managing containers
+    podman-compose # docker-compose alternative
 
     ## GUI apps
+    # notepad-next # notepad alternative
+    # fooyin # Music player # kaput in 25.11
+    # remmina # Remote Desktop Protocol client
     chromium # Web browser
     evolution # Email client
-    # fooyin # Music player # kaput in 25.11
     fsearch # Everything replacement
+    geeqie # Image viewer
+    ghostty # terminal
     gimp3 # Image editor
     gparted # Partition editor
-    geeqie # Image viewer
     hardinfo2 # Temperature and system information tool
     keepassxc # Password manager
     legcord # Discord client
     loupe # Simple image viewer
     mission-center # Task Manager
     mpv # Media player
-    notepad-next # notepad alternative
     pixelorama # Pixel art editor
-    spotify
-    # qalculate-qt # use Speedcrunch instead
     qbittorrent-enhanced # Torrent client
-    remmina # Remote Desktop Protocol client
+    qdirstat # Disk usage analyzer with Qt GUI
     rssguard # RSS reader
     signal-desktop # Signal messaging app
-    xnviewmp # Image viewer
     speedcrunch # Calculator
+    spotify
     synology-drive-client # Synology Drive client
     wezterm # Terminal emulator
     wireguard-tools # WireGuard tools
-    qdirstat # Disk usage analyzer with Qt GUI
+    xnviewmp # Image viewer
   ];
 
   home.activation.dlssUpdaterCleanup = lib.hm.dag.entryAfter ["writeBoundary"] ''
