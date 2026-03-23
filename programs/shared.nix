680d92b655833d53b5c101c913a67cda75ebb99b
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-03-23 21:13:06 +0100

    add git commands for walk, prev and next

--- programs/shared.nix
+++ programs/shared.nix
@@ -303,7 +303,7 @@ in {
     local.dpaint-js
     qbittorrent-enhanced # Torrent client
     qdirstat # Disk usage analyzer with Qt GUI
-    rssguard # RSS reader
+    rssguard # RSS  reader
     signal-desktop # Signal messaging app
     speedcrunch # Calculator
     spotify
