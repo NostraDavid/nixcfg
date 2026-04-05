6afa7a915e4ac2dd82fcf4c4496f02b025dc411f
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-04-05 18:41:46 +0200

    add local Austin

---
+++ pkgs/austin/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "austin";
+  version = "4.0.0";
+
+  src = fetchFromGitHub {
+    owner = "P403n1x87";
+    repo = "austin";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-FvYDlBYTdsUs42lsYQ5OUjzhvB5MCBFN6qsHToL4okA=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
+
+  meta = {
+    description = "Frame stack sampler for CPython";
+    homepage = "https://github.com/P403n1x87/austin";
+    changelog = "https://github.com/P403n1x87/austin/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.gpl3Plus;
+    mainProgram = "austin";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+})
