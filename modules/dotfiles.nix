d08c4032bb4ba3d1db43a3c49ee47bcf38eb2b8b
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-06-25 21:19:28 +0200

    project_color: is nu ook een python script

--- modules/dotfiles.nix
+++ modules/dotfiles.nix
@@ -47,6 +47,7 @@
       ".inputrc" = {source = mk "${dot}/readline/.inputrc";};
       ".local/bin/code" = {source = mk "${dot}/scripts/code";};
       ".local/bin/folder_stats" = {source = mk "${dot}/scripts/folder_stats";};
+      ".local/bin/project_color" = {source = mk "${dot}/scripts/project_color";};
       ".local/bin/venv" = {source = mk "${dot}/scripts/venv";};
       ".tmux.conf" = {source = mk "${dot}/tmux-3.5a/.tmux.conf";};
       ".vim" = {source = mk "${dot}/vim-9.0/.vim";};
