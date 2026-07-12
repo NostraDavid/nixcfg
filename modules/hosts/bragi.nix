0792fd35f38d96efb433b6ffa9c298c9b1b6f191
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-07-13 00:19:35 +0200

    bragi: beperk opslaggebruik van Nix

--- modules/hosts/bragi.nix
+++ modules/hosts/bragi.nix
@@ -17,11 +17,19 @@
 
       nix.settings = {
         cores = 10;
+        keep-derivations = lib.mkForce false;
+        keep-outputs = lib.mkForce false;
         max-jobs = lib.mkForce 10;
+        min-free = 2 * 1024 * 1024 * 1024;
+        max-free = 8 * 1024 * 1024 * 1024;
       };
+      nix.gc.options = lib.mkForce "--delete-older-than 7d";
       system.stateVersion = "25.05";
 
-      boot.kernelPackages = pkgs.linuxPackages_latest;
+      boot = {
+        kernelPackages = pkgs.linuxPackages_latest;
+        loader.grub.configurationLimit = 3;
+      };
 
       services = {
         xserver.xkb.options = "grp:win_space_toggle";
