4bbea74742e82c03a632fea7bb59fb9377c2a82b
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-07-17 11:02:36 +0200

    desktop-apps: laat MIME-associaties door apps beheren

--- modules/home/desktop-apps.nix
+++ modules/home/desktop-apps.nix
@@ -1,6 +1,4 @@
-{pkgs, ...}: let
-  geeqieDesktop = "org.geeqie.Geeqie.desktop";
-in {
+{pkgs, ...}: {
   home.packages = with pkgs; [
     fsearch
     geeqie
@@ -15,34 +13,4 @@ in {
     synology-drive-client
     wireguard-tools
   ];
-
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "image/avif" = geeqieDesktop;
-      "image/bmp" = geeqieDesktop;
-      "image/gif" = geeqieDesktop;
-      "image/heic" = geeqieDesktop;
-      "image/heif" = geeqieDesktop;
-      "image/jpeg" = geeqieDesktop;
-      "image/png" = geeqieDesktop;
-      "image/svg" = geeqieDesktop;
-      "image/svg+xml" = geeqieDesktop;
-      "image/tiff" = geeqieDesktop;
-      "image/webp" = geeqieDesktop;
-      "image/x-bmp" = geeqieDesktop;
-      "image/x-ico" = geeqieDesktop;
-      "image/x-icon" = geeqieDesktop;
-      "image/x-MS-bmp" = geeqieDesktop;
-      "image/x-png" = geeqieDesktop;
-      "image/x-portable-anymap" = geeqieDesktop;
-      "image/x-portable-bitmap" = geeqieDesktop;
-      "image/x-portable-graymap" = geeqieDesktop;
-      "image/x-portable-pixmap" = geeqieDesktop;
-      "image/x-tga" = geeqieDesktop;
-      "image/x-xbitmap" = geeqieDesktop;
-      "image/x-xpixmap" = geeqieDesktop;
-      "image/xpm" = geeqieDesktop;
-    };
-  };
 }
