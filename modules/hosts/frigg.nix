95084e5d7b987482b76af0ba5c7162b8494ffa09
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-07-21 08:43:24 +0200

    frigg: voeg ChatGPT-webapp toe

--- modules/hosts/frigg.nix
+++ modules/hosts/frigg.nix
@@ -5,7 +5,12 @@
 }: {
   flake.nixosConfigurations.frigg = mkHost {
     hostname = "frigg";
-    module = {lib, ...}: {
+    module = {
+      lib,
+      main-user,
+      pkgs,
+      ...
+    }: {
       imports = with config.flake.modules.nixos; [
         ../../hosts/frigg/hardware-configuration.nix
         workstation
@@ -17,6 +22,18 @@
         max-jobs = lib.mkForce 2;
       };
       systemd.services.nix-daemon.serviceConfig.CPUQuota = "400%";
+
+      home-manager.users.${main-user}.xdg.desktopEntries.chatgpt = {
+        name = "ChatGPT";
+        genericName = "AI Assistant";
+        comment = "Open ChatGPT in app mode";
+        exec = "${lib.getExe pkgs.chromium} --class=ChatGPT --app=https://chatgpt.com/";
+        icon = "chatgpt";
+        terminal = false;
+        categories = ["Network" "Utility"];
+        startupNotify = true;
+        settings.StartupWMClass = "ChatGPT";
+      };
 
       system.stateVersion = "25.05";
     };
