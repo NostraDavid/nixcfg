bc26225495f2c47cd9c1bb9a643aa4c470863259
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-07-26 10:58:17 +0200

    keyboard: map CapsLock to F13
    No more opening Settings by accident!

--- modules/home/keyboard.nix
+++ modules/home/keyboard.nix
@@ -6,6 +6,6 @@ _: {
     LayoutList=us,runic
     Layout=us
     VariantList=,basic
-    Options=caps:escape_shifted_compose,lv3:ralt_switch_multikey,compose:ralt,compose:rctrl,mod_led:compose,grp:win_space_toggle
+    Options=caps:f13,lv3:ralt_switch_multikey,compose:ralt,compose:rctrl,mod_led:compose,grp:win_space_toggle
   '';
 }
