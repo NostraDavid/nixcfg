8848f93696ceef86c63ab1508155d66a6885bdea
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2026-08-02 21:11:16 +0200

    probe: herstel packaging na dependency-update

--- pkgs/probe/default.nix
+++ pkgs/probe/default.nix
@@ -9,7 +9,8 @@ buildNpmPackage (finalAttrs: {
   version = "0.6.0-rc325";
 
   src = ./.;
-  npmDepsHash = "sha256-HSU6nu6smBxXu8u1KGDcINZrRT5Ui8/C6Psu6UrKcqI=";
+  npmDepsHash = "sha256-QWbZLO2QR7/g4X+S8FqUkJHoyru84a7aOZ5PspDPhXE=";
+  npmRebuildFlags = ["--ignore-scripts"];
   makeCacheWritable = true;
 
   dontNpmBuild = true;
@@ -18,6 +19,17 @@ buildNpmPackage (finalAttrs: {
 
   installPhase = ''
     runHook preInstall
+
+    substituteInPlace \
+      node_modules/@probelabs/probe/build/downloader.js \
+      node_modules/@probelabs/probe/build/extractor.js \
+      node_modules/@probelabs/probe/src/downloader.js \
+      node_modules/@probelabs/probe/src/extractor.js \
+      --replace-fail "import tar from 'tar';" "import * as tar from 'tar';"
+    (
+      cd node_modules/@probelabs/probe
+      node scripts/postinstall.js
+    )
 
     mkdir -p $out/lib/probe $out/bin
     cp -r node_modules $out/lib/probe/
